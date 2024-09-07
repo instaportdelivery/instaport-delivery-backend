3c9a96e1435a3782de440d711ea9922fb4dd34c9
mrudulkolambe <mrudul@vadavision.ca>
2024-09-07 18:14:59 +0530

    env change

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -7,12 +7,19 @@ const { v4: uuidv4 } = require('uuid');
 const { CustomerToken } = require("../Middlewares/CustomerAuth");
 const { RiderToken } = require("../Middlewares/RiderAuth");
 
+const header = {
+	"alg": "HS256",
+	"typ": "JWT",
+	"clientid": `${process.env.CLIENT_ID}`
+};
+
+const secretKey = `${process.env.SERVER_SECRET}`;
+
 router.post("/create-order/upi", CustomerToken, async (req, res) => {
 
 	const transaction_id = uuidv4();
 	const jwt_payload = {
-		"mercid": "UATINSPTV2",
-		// "mercid": "INSTAPRTV2",
+		"mercid": `${process.env.MERCHANT_ID}`,
 		"orderid": transaction_id,
 		"amount": Math.round(req.body.amount),
 		"order_date": "2023-07-30T20:25:00+05:30",
@@ -20,7 +27,7 @@ router.post("/create-order/upi", CustomerToken, async (req, res) => {
 		"additional_info": {
 			"additional_info1": `${req.customer._id}`,
 		},
-		"ru": "http://localhost:1000/authtest",
+		"ru": "https://instaportdelivery.in/authtest",
 		"itemcode": "DIRECT",
 		"device": {
 			"init_channel": "internet",
@@ -37,17 +44,6 @@ router.post("/create-order/upi", CustomerToken, async (req, res) => {
 			"browser_javascript_enabled": "true"
 		}
 	}
-	const secretKey = '31MhbX6UsCr7io5GJltm7kXsbbnxs7KO';
-
-
-
-
-	const header = {
-		"alg": "HS256",
-		"typ": "JWT",
-		"clientid": "uatinsptv2"
-		// "clientid": "instaprtv2"
-	};
 
 	const encodedHeader = btoa(JSON.stringify(header)).replace(/\+/g, '-').replace(/\//g, '_');
 	const encodedPayload = btoa(JSON.stringify(jwt_payload)).replace(/\+/g, '-').replace(/\//g, '_');
@@ -77,8 +73,7 @@ router.post("/create-order/upi", CustomerToken, async (req, res) => {
 		.then(async (result) => {
 			const data = await jwt.verify(result, secretKey)
 			const transaction_payload = {
-				"mercid": "UATINSPTV2",
-				// "mercid": "INSTAPRTV2",
+				"mercid": `${process.env.MERCHANT_ID}`,
 				"orderid": transaction_id,
 			}
 			const encodedPayloadTransaction = btoa(JSON.stringify(transaction_payload)).replace(/\+/g, '-').replace(/\//g, '_');
@@ -86,7 +81,7 @@ router.post("/create-order/upi", CustomerToken, async (req, res) => {
 			const signature = CryptoJS.HmacSHA256(signingString, secretKey).toString(CryptoJS.enc.Base64);
 			const encodedSignature = signature.replace(/\+/g, '-').replace(/\//g, '_').replace("=", "");
 			const jws = `${encodedHeader}.${encodedPayloadTransaction}.${encodedSignature}`;
-			fetch("https://uat1.billdesk.com/u2/payments/ve1_2/transactions/get", {
+			fetch("https://api.billdesk.com/payments/ve1_2/transactions/get", {
 				method: 'POST',
 				headers: {
 					"Content-Type": "application/jose",
@@ -108,13 +103,6 @@ router.post("/create-order/upi", CustomerToken, async (req, res) => {
 						extra: transaction_response
 					})
 				})
-
-			// res.json({
-			// 	bdorderid: data.bdorderid,
-			// 	orderid: data.orderid,
-			// 	authorization: data?.links[1].headers.authorization,
-			// 	amount: data.amount
-			// })
 		})
 		.catch(error => console.log('error', error));
 
@@ -147,9 +135,7 @@ router.post("/topup-wallet", CustomerToken, async (req, res) => {
 	const transaction_id = uuidv4();
 	console.log(req.body.ip, "ip")
 	const jwt_payload = {
-		// "mercid": "UATINSPTV2",
-		"mercid": "INSTAPRTV2",
-		// "mercid": "INSTAPRIDR",
+		"mercid": `${process.env.MERCHANT_ID}`,
 		"orderid": transaction_id,
 		"amount": `${req.body.amount}.00`,
 		"order_date": customFormattedDate,
@@ -158,7 +144,7 @@ router.post("/topup-wallet", CustomerToken, async (req, res) => {
 			"additional_info1": `${req.customer._id}`,
 		},
 		// "ru": "http://localhost:1000/customer-transactions/wallet-topup",
-		"ru": "http://13.202.178.247:1000/customer-transactions/wallet-topup",
+		"ru": "https://instaportdelivery.in/customer-transactions/wallet-topup",
 		"itemcode": "DIRECT",
 		"device": {
 			"init_channel": "internet",
@@ -175,15 +161,6 @@ router.post("/topup-wallet", CustomerToken, async (req, res) => {
 			"browser_javascript_enabled": "true"
 		}
 	}
-	const secretKey = 'F1kyMNGYF1BHl4L8AZtHRaadCoZrQJqy';
-	// const secretKey = '6QyIqweGQBJH6t1pfBVnSE8ItaWskzk5';
-
-	const header = {
-		"alg": "HS256",
-		"typ": "JWT",
-		// "clientid": "uatinsptv2"
-		"clientid": "instaprtv2"
-	};
 
 	const encodedHeader = btoa(JSON.stringify(header)).replace(/\+/g, '-').replace(/\//g, '_');
 	const encodedPayload = btoa(JSON.stringify(jwt_payload)).replace(/\+/g, '-').replace(/\//g, '_');
@@ -211,7 +188,6 @@ router.post("/topup-wallet", CustomerToken, async (req, res) => {
 
 	console.log(headers, requestOptions)
 
-	// fetch("https://uat1.billdesk.com/u2/payments/ve1_2/orders/create", requestOptions)
 	fetch("https://api.billdesk.com/payments/ve1_2/orders/create", requestOptions)
 		.then(response => response.text())
 		.then(async (result) => {
@@ -258,8 +234,7 @@ router.post("/payment-order", CustomerToken, async (req, res) => {
 	let formattedDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}${offsetSign}${String(offsetHours).padStart(2, '0')}:${String(offsetMinutes).padStart(2, '0')}`;
 
 	const jwt_payload = {
-		// "mercid": "UATINSPTV2",
-		"mercid": "INSTAPRTV2",
+		"mercid": `${process.env.MERCHANT_ID}`,
 		"orderid": transaction_id,
 		"amount": req.body.amount,
 		"order_date": formattedDate,
@@ -267,8 +242,7 @@ router.post("/payment-order", CustomerToken, async (req, res) => {
 		"additional_info": {
 			"additional_info1": `${req.customer._id}`,
 		},
-		// "ru": `http://localhost:1000/customer-transactions/app-create-payment/${req.body.order}`,
-		"ru": `http://13.202.178.247:1000/customer-transactions/app-create-payment/${req.body.order}`,
+		"ru": `https://instaportdelivery.in/customer-transactions/app-create-payment/${req.body.order}`,
 		"itemcode": "DIRECT",
 		"device": {
 			"init_channel": "internet",
@@ -285,14 +259,6 @@ router.post("/payment-order", CustomerToken, async (req, res) => {
 			"browser_javascript_enabled": "true"
 		}
 	}
-	const secretKey = 'F1kyMNGYF1BH14L8AZtHRaadCoZrQJqy';
-
-	const header = {
-		"alg": "HS256",
-		"typ": "JWT",
-		// "clientid": "uatinsptv2"
-		"clientid": "instaprtv2"
-	};
 
 	const encodedHeader = btoa(JSON.stringify(header)).replace(/\+/g, '-').replace(/\//g, '_');
 	const encodedPayload = btoa(JSON.stringify(jwt_payload)).replace(/\+/g, '-').replace(/\//g, '_');
@@ -316,7 +282,6 @@ router.post("/payment-order", CustomerToken, async (req, res) => {
 		body: jws,
 	};
 
-	// fetch("https://uat1.billdesk.com/u2/payments/ve1_2/orders/create", requestOptions)
 	fetch("https://api.billdesk.com/payments/ve1_2/orders/create", requestOptions)
 		.then(response => response.text())
 		.then(async (result) => {
@@ -355,8 +320,7 @@ router.post("/payment-dues-rider", RiderToken, async (req, res) => {
 	let formattedDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}${offsetSign}${String(offsetHours).padStart(2, '0')}:${String(offsetMinutes).padStart(2, '0')}`;
 
 	const jwt_payload = {
-		// "mercid": "UATINSPTV2",
-		"mercid": "INSTAPRTV2",
+		"mercid": `${process.env.MERCHANT_ID}`,
 		"orderid": transaction_id,
 		"amount": Math.abs(Number(req.body.amount)),
 		"order_date": formattedDate,
@@ -365,14 +329,13 @@ router.post("/payment-dues-rider", RiderToken, async (req, res) => {
 			"additional_info1": `${req.rider._id}`,
 		},
 		// "ru": `http://localhost:1000/rider/app-payment`,
-		"ru": `http://13.202.178.247:1000/rider/app-payment`,
+		"ru": `https://instaportdelivery.in/rider/app-payment`,
 		"itemcode": "DIRECT",
 		"device": {
 			"init_channel": "internet",
 			"ip": "192.168.0.104",
 			"user_agent": req.body.user_agent,
 			"accept_header": "text/html",
-			"fingerprintid": "61b12c18b5d0cf901be34a23ca64bb19",
 			"browser_tz": "-330",
 			"browser_color_depth": "32",
 			"browser_java_enabled": "false",
@@ -382,14 +345,6 @@ router.post("/payment-dues-rider", RiderToken, async (req, res) => {
 			"browser_javascript_enabled": "true"
 		}
 	}
-	const secretKey = 'F1kyMNGYF1BH14L8AZtHRaadCoZrQJqy';
-
-	const header = {
-		"alg": "HS256",
-		"typ": "JWT",
-		// "clientid": "uatinsptv2"
-		"clientid": "instaprtv2"
-	};
 
 	const encodedHeader = btoa(JSON.stringify(header)).replace(/\+/g, '-').replace(/\//g, '_');
 	const encodedPayload = btoa(JSON.stringify(jwt_payload)).replace(/\+/g, '-').replace(/\//g, '_');
